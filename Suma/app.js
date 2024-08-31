836c8285d6c885a93436aeda971cc3d32e527b93
DanielBermeo128 <dbermeo128@gmail.com>
2024-08-31 12:40:51 -0600

    Agregar practica de suma con JS

---
+++ Suma/app.js
@@ -0,0 +1,5 @@
+// Crear un porograma que pida dos numeros y que los sume
+function sumar(a=prompt("Dame el primer numero"),b=prompt("Dame el segundo numero")){
+	return parseInt(a)+parseInt(b);
+}
+console.log(sumar());
